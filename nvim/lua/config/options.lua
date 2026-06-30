6d3f62e9c23592ab0bdc8830d2a7696f50c414af
azizu06 <ab725492@ucf.edu>
2026-06-30 17:22:26 -0400

    nvim tweaks: U=redo, re-enable :Tutor, disable whitespace markers, Copilot extra, vim-be-good

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Don't show whitespace markers (the "-" at trailing spaces, ">" at tabs).
+-- Format-on-save already strips trailing whitespace, so this is just visual noise.
+vim.opt.list = false
